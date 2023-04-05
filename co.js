7a83282315ff0291c87a6415a069c40acd2fcf37
Vaibhav-T0mar <129948846+Vaibhav-T0mar@users.noreply.github.com>
2023-04-05 15:39:23 +0530

    Update co.js

--- co.js
+++ co.js
@@ -1,27 +1,16 @@
 const body = document.body;
 const button = document.querySelector('button');
 console.log(body);
-// const red = Math.floor(Math.random()*256);
-// const green = Math.floor(Math.random()*256);
-// const blue = Math.floor(Math.random()*256);
-// const rgb =`${red},${green},${blue}`;
-// const set = "rgb("+rgb+")"
-// bo.style.backgroundColor = set; 
-
+ 
 const id = setInterval(()=>{
     const red = Math.floor(Math.random()*256);
     const green = Math.floor(Math.random()*256);
     const blue = Math.floor(Math.random()*256);
-    const rgb =`rgb(${red},${green},${blue})`;
-    // const set = "rgb("+rgb+")"
+    const rgb =`rgb(${red},${green},${blue})`; 
     console.log(rgb)
     body.style.backgroundColor = rgb
 },1000);
 console.log(id)
-// clearInterval(1);
-
-
-
 button.addEventListener("click", ()=>{
     clearInterval(id);
     button.textContent = body.style.backgroundColor;
@@ -29,8 +18,5 @@ button.addEventListener("click", ()=>{
 })
 
 button.addEventListener("dblclick", ()=>{
-    // clearInterval(id);
     button.textContent = 'you tab two times'
-    // body.style.backgroundColor = rgb
-    
 })
